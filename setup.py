841ead174f796c68831ce1c78bfc35e44aec73c9
wu <wu@.com>
2025-09-01 19:11:10 +0800

    run serge-sans-paille/gast/gpt-4o-3

--- setup.py
+++ setup.py
@@ -1,60 +1,43 @@
-import sys
-
+# Use the newer `setuptools.setup()`, if available.
 try:
     from setuptools import setup
-    using_setuptools = True
+    kw = {
+        'test_suite': 'tests',
+        'tests_require': ['astunparse'],
+    }
 except ImportError:
     from distutils.core import setup
-    using_setuptools = False
+    kw = {}
 
-from os.path import join, dirname
+setup(name='gast',  # gast, daou naer!
+      version='0.4.0',
+      packages=['gast'],
+      description='Python AST that abstracts the underlying Python version',
+      long_description='''
+A generic AST to represent Python2 and Python3's Abstract Syntax Tree(AST).
 
-with open(join(dirname(__file__), 'scrapyd/VERSION')) as f:
-    version = f.read().strip()
-
-setup_args = {
-    'name': 'scrapyd',
-    'version': version,
-    'url': 'https://github.com/scrapy/scrapyd',
-    'description': 'A service for running Scrapy spiders, with an HTTP API',
-    'long_description': open('README.rst').read(),
-    'author': 'Scrapy developers',
-    'maintainer': 'Scrapy developers',
-    'maintainer_email': 'info@scrapy.org',
-    'license': 'BSD',
-    'packages': ['scrapyd'],
-    'include_package_data': True,
-    'zip_safe': False,
-    'classifiers': [
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Environment :: No Input/Output (Daemon)',
-        'Topic :: Internet :: WWW/HTTP',
-    ],
-}
-
-
-if using_setuptools:
-    setup_args['install_requires'] = [
-        'Twisted>=8.0',
-        'Scrapy>=1.0',
-        'six',
-        'enum-compat',
-    ]
-    setup_args['entry_points'] = {'console_scripts': [
-        'scrapyd = scrapyd.scripts.scrapyd_run:main'
-    ]}
-else:
-    setup_args['scripts'] = ['scrapyd/scripts/scrapyd_run.py']
-
-setup(**setup_args)
+GAST provides a compatibility layer between the AST of various Python versions,
+as produced by ``ast.parse`` from the standard ``ast`` module.''',
+      author='serge-sans-paille',
+      author_email='serge.guelton@telecom-bretagne.eu',
+      url='https://github.com/serge-sans-paille/gast/',
+      license="BSD 3-Clause",
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Console',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Natural Language :: English',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6',
+                   'Programming Language :: Python :: 3.7',
+                   'Programming Language :: Python :: 3.8',
+                   'Programming Language :: Python :: 3.9',
+                   'Programming Language :: Python :: 3.10',
+                   ],
+      python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+      **kw
+      )
