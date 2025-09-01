79223b28a30abdd93a3e84d3842eab9baf005ab3
wu <wu@.com>
2025-09-01 18:58:28 +0800

    run matplotlib/cycler/gpt-4o-3

--- setup.py
+++ setup.py
@@ -1,51 +1,23 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 from setuptools import setup
 
-import feedgen.version
-
-packages = ['feedgen', 'feedgen/ext']
-
-setup(name='feedgen',
-      packages=packages,
-      version=feedgen.version.version_full_str,
-      description='Feed Generator (ATOM, RSS, Podcasts)',
-      author='Lars Kiesow',
-      author_email='lkiesow@uos.de',
-      url='https://lkiesow.github.io/python-feedgen',
-      keywords=['feed', 'ATOM', 'RSS', 'podcast'],
-      license='FreeBSD and LGPLv3+',
-      install_requires=['lxml', 'python-dateutil'],
-      classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',
-        'License :: OSI Approved :: GNU Lesser General Public License v3 ' +
-        'or later (LGPLv3+)',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
-        'Topic :: Communications',
-        'Topic :: Internet',
-        'Topic :: Text Processing',
-        'Topic :: Text Processing :: Markup',
-        'Topic :: Text Processing :: Markup :: XML'
-        ],
-      test_suite="tests",
-      long_description='''\
-Feedgenerator
-=============
-
-This module can be used to generate web feeds in both ATOM and RSS format. It
-has support for extensions. Included is for example an extension to produce
-Podcasts.
-
-It is licensed under the terms of both, the FreeBSD license and the LGPLv3+.
-Choose the one which is more convenient for you. For more details have a look
-at license.bsd and license.lgpl.
-''')
+setup(name='cycler',
+      version='0.10.0',
+      author='Thomas A Caswell',
+      author_email='matplotlib-users@python.org',
+      py_modules=['cycler'],
+      description='Composable style cycles',
+      url='https://github.com/matplotlib/cycler',
+      platforms='Cross platform (Linux, Mac OSX, Windows)',
+      license="BSD",
+      python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+      classifiers=['License :: OSI Approved :: BSD License',
+                   'Development Status :: 4 - Beta',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6',
+                   ],
+      keywords='cycle kwargs',
+      )
